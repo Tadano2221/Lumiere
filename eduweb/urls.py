b4056ef0052f8f8d7a13074e3ab7638a1460b4cd
Ted <90366198+Tadano2221@users.noreply.github.com>
2024-02-06 21:24:34 +0630

    Update urls.py

--- eduweb/urls.py
+++ eduweb/urls.py
@@ -1,6 +1,13 @@
-from . import views
-from django.urls import path
+from django.shortcuts import render
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('', views.index, name='index')
-]
+# Create your views here.
+
+def index(request):
+    return render(request, 'eduweb/index.html')
+
+def chat(request):
+    return render(request, 'eduweb/chat.html')
+
+def room(request, room_name):
+    return render(request, "eduweb/room.html", {"room_name": room_name})
